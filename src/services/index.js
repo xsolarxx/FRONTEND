01ae73463ca723305c05b1ccf6be8492379b96a4
Adrian <ortizdev@proton.me>
2024-02-26 19:32:41 +0100

    Exporting services

--- src/services/index.js
+++ src/services/index.js
@@ -1,1 +1,5 @@
-export * from "./serviceApiGeneral.conf";
+export * from "./serviceApiGeneral.config";
+export * from "./comment.service";
+export * from "./company.service";
+export * from "./forum.service";
+export * from "./user.service";

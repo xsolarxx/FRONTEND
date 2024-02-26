f9fe5f9aea4757d93379eb9a3ba31d2c01e0810d
Adrian <ortizdev@proton.me>
2024-02-26 20:44:47 +0100

    stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Footer, ImageHome } from "./components";
+import { Header } from "./components/Header/Header";
 import { Home } from "./pages";
 
 
 const App = () => {
   return (
     <>
+      <Header/>
       <ImageHome/>
       <Home/>
       <Footer/>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,14 @@ import { Nav } from "../Navigation/Nav";
 
 export const Header = () => {
   return (
-    <header>
+    <header className ="header">
       <Nav />
+      <div className="video-container">
+        <video className="header-video" autoPlay loop muted>
+          <source src="/wind.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </header>
   );
 };

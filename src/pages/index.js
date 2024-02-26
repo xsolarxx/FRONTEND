acb024c5e17a4f0bf67fdcf90ee240f86c298d55
Monique Mendes <moniiquemendes96@gmail.com>
2024-02-26 13:34:58 +0100

    Routes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 export * from "./Home/Home.jsx";
-export * from "./Dashboard/Dashboard.jsx";
+
 export * from "./Forum/Forum.jsx";
 export * from "./News/News.jsx";
 export * from "./Company/Company.jsx";
-export * from "./";
+export * from "./Dashboard/Dashboard.jsx";
 export * from "./";
 export * from "./";
 export * from "./";
---
+++ src/routes/routes.jsx
@@ -0,0 +1,17 @@
+import App from "../App";
+
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "../pages";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);

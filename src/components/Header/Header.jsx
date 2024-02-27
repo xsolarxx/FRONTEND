adfe3a2d2b2ce38fa04789149f8976374af852d2
InesMedem <inesmedem93@gmail.com>
2024-02-27 15:36:36 +0100

    carpetas para el Login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import "./Header.css";
 import { Nav } from "../Navigation/Nav";
 
+// tenemos que hacer un conditional rendering en el header para que muestre mas cosas si el usuario esta logado
+
 export const Header = () => {
   return (
     <header className="header">
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,8 @@
+// export default function Login() {
+//   const { register, handlehandleSubmit } = useForm();
+//   /**
+//    * registor -> nombre y valor, para el input
+//    * handleSubmit -> va a devolver un objecto con todos los eleentos registrados
+//    */
+//   return <div>Login</div>;
+// }

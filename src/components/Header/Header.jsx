fc1bdd61f5964161a73452f5b93e84151219e209
InesMedem <inesmedem93@gmail.com>
2024-02-28 18:14:03 +0100

    cambios al css de home y header, y configs en root

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,47 +8,24 @@ export const Header = () => {
   const { user, logout } = useAuth();
   return (
     <>
-      <header className="max-width-container">
-        <div className="titleFatherContainer">
-          <img
-            src="https://media.discordapp.net/attachments/1201292840450404382/1212154696673005638/image.png?ex=65f0ce08&is=65de5908&hm=5fda8a6bb1decf8f1933607fd8f7ebe5da2f8f35b3e3ac10031a3e80decb9325&=&format=webp&quality=lossless&width=417&height=417"
-            alt="logo"
-            className="logo"
-          />
-          <div className="titleContainer">
-            <h4 className="titleHeaderBlack">USER PAGE</h4>
-          </div>
-        </div>
+      <header>
         <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/dashboard">Dashboard</NavLink>
+          <NavLink to="/company">Company</NavLink>
+          <NavLink to="/news">News</NavLink>
+
           {user == null && (
             <NavLink to="/login">
-              <img
-                src="https://media.discordapp.net/attachments/1201292840450404382/1212155952997072986/image.png?ex=65f0cf33&is=65de5a33&hm=a11064b401fbc0896e7abed448dbe3572e55d115575181c75ab54218caafe0bd&=&format=webp&quality=lossless&width=417&height=417"
-                alt=""
-                className="iconNav"
-              />
+              <span className="material-symbols-outlined">logout</span>
             </NavLink>
           )}
 
-          {user !== null ? (
-            <NavLink to="/dashboard">
-              <img
-                src="https://res.cloudinary.com/dq186ej4c/image/upload/v1685705689/dashboard-statistics-5492_rnmxcl.png"
-                alt=""
-                className="iconNav iconDashBoard"
-              />
-            </NavLink>
-          ) : null}
-
-          <NavLink to="/">
-            <img
-              src="https://media.discordapp.net/attachments/1201292840450404382/1212154939879592116/image.png?ex=65f0ce42&is=65de5942&hm=dce3dbcf25894eddf48154df82aea23bb44b80cdc4227b09d63608a0697c25ad&=&format=webp&quality=lossless&width=417&height=417"
-              alt=""
-              className="iconNav home"
-            />
-          </NavLink>
+          {user !== null ? <NavLink to="/dashboard">Dashboard</NavLink> : null}
+          {user !== null ? <NavLink to="/company">Dashboard</NavLink> : null}
+          {user !== null ? <NavLink to="/news">Dashboard</NavLink> : null}
           {user !== null && (
-            <button className="iconNav iconLogout" onClick={() => logout()}>
+            <button className="icon" onClick={() => logout()}>
               <img
                 src="https://media.discordapp.net/attachments/1201292840450404382/1212156762640359514/image.png?ex=65f0cff4&is=65de5af4&hm=20c39b23b257bb87f871a83ffa6ba452fab47eaadef9de037eebbd052f69e354&=&format=webp&quality=lossless&width=417&height=417"
                 alt="Foto"
@@ -56,16 +33,12 @@ export const Header = () => {
             </button>
           )}
           {user !== null ? (
-            <>
-              <NavLink to="/profile">
-                <img className="profileCircle" src={user.image} alt={user.user} />
-              </NavLink>
-            </>
+            <NavLink to="/profile">
+              <img className="icon" src={user.image} alt={user.user} />
+            </NavLink>
           ) : null}
-          {}
         </nav>
       </header>
-      <div className="whiteContainer"></div>
     </>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,40 +2,26 @@ import './Home.css';
 
 export const Home = () => {
   return (
-    <div>
-      <div className="video-container">
-        <video className="header-video" autoPlay loop muted>
-          <source src="/wind.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>{' '}
+    <>
+      {/* <video className="header-video" autoPlay loop muted>
+        <source src="/wind.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video> */}
+
+      <div className="section1">
+        <h1>Transformando el Futuro con Energía Renovable </h1>
+        <h5>Conéctate con la Innovación Sostenible en xSolar</h5>
       </div>
 
-      <div className="home1">
-        <h1>Transformando el Futuro con Energía Renovable</h1>
-        <h3>Conéctate con la Innovación Sostenible en xSolar</h3>
+      <div className="section2">
+        <h2>¿alguna vez te has preguntado sobre las energéas renovables? </h2>
       </div>
 
-      <p>
-        As energias renováveis, incluindo solar, eólica, hidrelétrica, biomassa e
-        geotérmica, oferecem uma alternativa ecologicamente correta para a produção de
-        eletricidade. Ao aproveitar os recursos naturais disponíveis, podemos reduzir
-        significativamente as emissões de gases de efeito estufa, combater as mudanças
-        climáticas e garantir um futuro mais limpo para as gerações futuras.
-      </p>
-      <div>
-        <h2>O Que Você Encontrará?</h2>
-        <ul>
-          <li>Descubra o Seu Parceiro Energético</li>
-          <li>Encontre Empresas Renováveis</li>
-          <li>Junte-se ao Debate Verde</li>
-          <li>Participe do Nosso Fórum Energético</li>
-          <li>Mantenha-se Atualizado com as Últimas Notícias</li>
-        </ul>
+      <div className="section3">
+        <h2>¿Que encontraras? </h2>
+        <h4>Descubre tu Socio Energético: Encuentra Empresas Renovables</h4>
+        <h4>Descubre tu Socio Energético: Encuentra Empresas Renovables</h4>
       </div>
-      <p>
-        Explore nosso site para descobrir como você pode se envolver e contribuir para um
-        futuro mais sustentável e limpo!
-      </p>
-    </div>
+    </>
   );
 };

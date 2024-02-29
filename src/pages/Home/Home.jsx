af3bb3cddf646a47a2a8302286fde7d8445f0ddf
InesMedem <inesmedem93@gmail.com>
2024-02-29 09:06:27 +0100

    cambios css en footer , header y home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import './Home.css';
 
 import AnimatedHeading from '../../components/AnimationScroll/AnimationScroll';
+import { ImageHome } from '../../components/ImageHome/ImageHome';
 
 export const Home = () => {
   return (
@@ -14,8 +15,9 @@ export const Home = () => {
       <div className="section1">
         <h1>Transformando el Futuro con Energía Renovable </h1>
         <h5>Conéctate con la Innovación Sostenible en xSolar</h5>
+        <button>Find out more</button>
       </div>
-
+      <ImageHome />
       <div className="section2">
         <h2>¿Alguna vez te has preguntado sobre las energías renovables? </h2>
       </div>
@@ -23,6 +25,8 @@ export const Home = () => {
       <div className="section3">
         <AnimatedHeading />
       </div>
+
+      <div></div>
     </>
   );
 };

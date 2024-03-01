7693681cdcb4c8a65c298ca3c62b0a965bd761b4
Xiao Xue <anxxue@gmail.com>
2024-03-01 17:59:02 +0100

    changing login code

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -65,7 +65,7 @@ export const Login = () => {
 
   if (loginOk) {
     if (res.data?.user?.check == false) {
-      return <Navigate to="/dashboard" />;
+      return <Navigate to="/verifyCode" />;
     } else {
       return <Navigate to="/dashboard" />;
     }

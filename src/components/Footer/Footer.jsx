8ecfd2f2c6ceef7acb8425e134ece96dc425f451
Xiao Xue <anxxue@gmail.com>
2024-03-03 15:13:56 +0100

    Fixing footer + adding nav links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,30 +1,38 @@
 import './Footer.css';
+
+import { NavLink } from 'react-router-dom';
+
 export const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <div className="column-center">
-          <h2>Follow us</h2>
-          <p>Linkedin</p>
-          <p>Twitter</p>
-          <p>Instagram</p>
-          <p>Podcast</p>
+        <div className="follow-content">
+          <h4>Follow us</h4>
+          <ul>
+            <li><a href="/">Linkedin</a></li>
+            <li><a href="/">Twitter</a></li>
+            <li><a href="/">Instagram</a></li>
+            <li><a href="/">Podcast</a></li>
+          </ul>
         </div>
         <div className="column-center">
-          <h2 className="contacto">Contact</h2>
+          <h4>Contact</h4>
           <strong>
             <p>Email:</p>
           </strong>
           <p>xsolarxcompany@gmail.com</p>
-          <strong>Address:</strong>
-          <p>Plaza Jose Rinaldo,5 ,18882, Madrid</p>
+          <strong className="spacing">Address:</strong>
+          <p>Plaza José Rinaldo,5 ,18882, Madrid</p>
         </div>
         <div className="column-center">
-          <h2 className="navegacion">Navigation</h2>
-          <p>Home</p>
-          <p>News</p>
-          <p>Companies</p>
-          <p>Forum</p>
+          <h4>Navigation</h4>
+          <ul>
+            {/* Arreglar los estilos de estos navlink, el color no se puede modificar del mismo modo que con la lista de follow us */}
+            <li><NavLink className="links" to="/">Home</NavLink></li>
+            <li><NavLink className="links" to="/news">News</NavLink></li>
+            <li><NavLink className="links" to="/companies">Companies</NavLink></li>
+            <li><NavLink className="links" to="/forum">Forum</NavLink></li>
+          </ul>
         </div>
       </div>
       <p className="rights">2024 © All rights reserved</p>

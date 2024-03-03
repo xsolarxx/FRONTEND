56efdcc1763471098c1de89a2b47674613cfb5cd
InesMedem <inesmedem93@gmail.com>
2024-03-03 09:47:20 +0100

    home css arreglado

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import './Home.css';
-import '../../main';
 import AnimatedHeading from '../../components/AnimationScroll/AnimationScroll';
 import { ImageHome } from '../../components/ImageHome/ImageHome';
 
@@ -14,7 +13,7 @@ export const Home = () => {
         <button className="button--white button--large p-l">Get Started</button>
       </section>
 
-      <section className="big-titel-section">
+      <section className="big-titel-section p-35">
         <h1>
           Solarx operates in the renewable energies market. We were born green, not made
           green.
@@ -35,37 +34,11 @@ export const Home = () => {
           </p>
         </span>
       </section>
-      <div className="videoSection">
-        <video className="video" autoPlay loop muted>
-          <source src="/wind.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-      <div className="section1">
-        <h1>Transformando el Futuro con Energía Renovable </h1>
-        <h5>Conéctate con la Innovación Sostenible en xSolar</h5>
-        <button>Find out more</button>
-      </div>
+
       <ImageHome />
-      <div className="section2">
-        <h2>¿Alguna vez te has preguntado sobre las energías renovables? </h2>
-      </div>
 
       <div className="section3">
         <AnimatedHeading />
-      </div>
-
-      <div className="section4">
-        <div className="column1">
-          <h1> Global by Nature </h1>
-        </div>
-        <div className="column2">
-          <h6>
-            Greenvolt is increasingly recognised in the European market as a major player
-            in the promotion and development of renewable energy projects
-          </h6>
-          <button className="button2"> Sustainability </button>
-        </div>
       </div>
     </>
   );

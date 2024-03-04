9044ec5925f1ce2d6ce2a28c4e548b1b95b024be
InesMedem <inesmedem93@gmail.com>
2024-03-04 08:29:41 +0100

    changes to home grid

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import './Home.css';
 import '../../components/Buttons/button.css';
 
-import AnimatedHeading from '../../components/AnimationScroll/AnimationScroll';
 import { ImageHome } from '../../components/ImageHome/ImageHome';
 
 export const Home = () => {
@@ -12,50 +11,65 @@ export const Home = () => {
         <p className="p-1 p-l">
           Explore renewable companies and connect with industry professionals
         </p>
-        <button className="button--white button--large p-l button-hover">
-          Get Started
-        </button>
+        <button className="button--white button--large button-hover">Sign up</button>
       </section>
 
       <section className="big-titel-section p-25">
-        <h3>
+        <p>XSOLAREX</p>
+        <h2>
           Solarx operates in the renewable energies market. We were born green, not made
           green.
-        </h3>
+        </h2>
       </section>
 
       <section className="section-consumer p-25 gap-25">
         <span className="section-consumer__image"></span>
-
         <span className="section-consumer__text">
           <h3>Discover, connect and transform your relationship with renewable energy</h3>
           <h4>Your gateway to a greener future. </h4>
           <p className="p-l">
-            `At xSolar.com, we are dedicated to empowering individuals on their
-            sustainable journey. Through our platform, you can discover, connect, and
-            transform your relationship with renewable energy. Whether you are exploring
-            solar, wind, or other forms of renewable energy.`
+            At xSolar.com, we are dedicated to empowering individuals on their sustainable
+            journey. Through our platform, you can discover, connect, and transform your
+            relationship with renewable energy. Whether you are exploring solar, wind, or
+            other forms of renewable energy.
           </p>
         </span>
       </section>
 
-      <section className="grid p-35 gap-25">
-        <div className="grid-item1"></div>
+      <div className="grid-titel-with-subtitle">
+        <h3>What you will find inside our platform</h3>
+        <p className="p-xl p-1">
+          A community of like minded people and a platfom to learn and grow your interest
+          of renewables
+        </p>
+      </div>
+
+      <section className="grid p-35 gap-15">
+        <div className="grid-item1">
+          <h3>NEWS</h3>
+          {/* <p>Empresas que encajen con tu idea de sostenibilidad</p> */}
+        </div>
         <div className="grid-item2"></div>
-        <div className="grid-item3"></div>
-        <div className="grid-item4"></div>
+        <div className="grid-item3">
+          <h3>FORUM</h3>
+          {/* <p>Empresas que encajen con tu idea de sostenibilidad</p> */}
+        </div>
+        <div className="grid-item4">
+          <h3>COMPANIES</h3>
+          {/* <p>Empresas que encajen con tu idea de sostenibilidad</p> */}
+        </div>
         <div className="grid-item5"></div>
       </section>
 
       <section className="section-banner p-50">
-        <h2 className="p-35">Solar by Nature</h2>
+        <h1 className="p-35">Solar by Nature</h1>
         <div className="div-banner gap-15">
-          <p>
+          <p className="p-l">
             xSolarx is increasingly recognized in the European market as a major player in
             the promotion and development of renewable energy projects
           </p>
           <button className="button--white button--medium button-hover">
-            <strong>Discover more</strong>
+            Discover more
           </button>
         </div>
       </section>

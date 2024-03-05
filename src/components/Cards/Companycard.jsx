bbb51ca4ad59277d89bb4b29cfe2f17fe951bece
Monique Mendes <moniiquemendes96@gmail.com>
2024-03-05 18:41:11 +0100

    css company

--- src/components/Cards/Companycard.jsx
+++ src/components/Cards/Companycard.jsx
@@ -2,12 +2,16 @@ import './CompanyCard.css';
 
 export const CompanyCard = ({ company }) => {
   return (
-    <div className="containerCompany">
-      <h1 className="companyName">{company.companyName} </h1>
-      <p className="companyDescription"> {company.description}</p>
-      <img className="imgCompany" src={company.image} alt={company.name} />
-      <h2 className="companyType"> {company.type}</h2>
-      <p className="companyServices">{company.companyServices}</p>
-    </div>
+    <>
+      <div className="page-container-card">
+        <div className="containerCompany">
+          <h3 className="companyName">{company.companyName} </h3>
+          <p className="companyDescription"> {company.description}</p>
+          <img className="imgCompany" src={company.image} alt={company.name} />
+          <h2 className="companyType"> {company.type}</h2>
+          <p className="companyServices">{company.companyServices}</p>
+        </div>
+      </div>
+    </>
   );
 };

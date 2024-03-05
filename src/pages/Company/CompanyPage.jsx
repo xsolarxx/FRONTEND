8340d4e778043e0ff9de4f4f6142cfa25089c78b
Monique Mendes <moniiquemendes96@gmail.com>
2024-03-05 12:51:15 +0100

    Mapeo company

--- src/pages/Company/CompanyPage.jsx
+++ src/pages/Company/CompanyPage.jsx
@@ -13,22 +13,22 @@ export const CompanyPage = () => {
   //*antes de hacer el mapeo tenemos que ver si la longitd de la mapa es mayor que 0
   //* si no se va a rompero elmapeo , si es mayor que 0 pintamos las compañias
   //* Si es menor que 0 --> sin compañias que mostrar
-console.log(companyList);
-  return (
-    <>
-      {{companyList.data.map((company) => {
-        if (company) {
-          return (
-            <div className="companyName" key={company.companyName}>
-              <p className="companyDescription"> key={company.description}</p>
-              <img className="imgCompany" src={company.image} alt={company.name} />
-              <h2 className="companyType"> {company.type}</h2>
-              <p className="companyServices">Status : {company.companyServices}</p>
-            </div>
-          );
-        }
-        return null;
-      })}}
-    </>
-  );
+
+  //* Verificar se a lista de empresas tem itens antes de mapeá-la
+  if (companyList.data && companyList.data.length > 0) {
+    return (
+      <>
+        {companyList.data.map((company) => (
+          <div className="companyName" key={company.companyName}>
+            <p className="companyDescription"> {company.description}</p>
+            <img className="imgCompany" src={company.image} alt={company.name} />
+            <h2 className="companyType"> {company.type}</h2>
+            <p className="companyServices">{company.companyServices}</p>
+          </div>
+        ))}
+      </>
+    );
+  } else {
+    return <p>No companies to display</p>;
+  }
 };

fb5b1f043f03075e2e48a0b61cccd24cfa7bf2e4
Monique Mendes <moniiquemendes96@gmail.com>
2024-03-08 00:48:17 +0100

    En progreso css company

--- src/components/Cards/ComapanyDetailCard.jsx
+++ src/components/Cards/ComapanyDetailCard.jsx
@@ -1,11 +1,11 @@
-import './CompanyCard.css';
+import './ComapanyDetailCard.css';
 
 // el componente para mostrar todo el contenido
 // sera importado en la CompanyDetail.jsx
 export const CompanyDetailCard = ({ company }) => {
   return (
     <div className="containerCompany">
-      <h3 className="companyName">Name: {company.companyName} </h3>
+      <p className="companyName">Name: {company.companyName} </p>
       <p className="companyDescription">description: {company.description}</p>
       <img className="imgCompany" src={company.image} alt={company.name} />
       <h2 className="companyType"> Type:{company.type}</h2>
--- src/components/Cards/CompanyCard.jsx
+++ src/components/Cards/CompanyCard.jsx
@@ -5,20 +5,24 @@ import { LikeCompany } from '../LikeButtonCompany/LikeButton';
 export const CompanyCard = ({ company }) => {
   const navigate = useNavigate();
   return (
-    <div className="containerCompany">
-      <h3 className="companyName">Name: {company.companyName} </h3>
-      <img className="imgCompany" src={company.image} alt={company.name} />
-      <h2 className="companyType"> Type:{company.type}</h2>
-      <p className="companyServices">Services offered:{company.companyServices}</p>
-      <LikeCompany id={company._id} />
-      <button
-        className="button--blue"
-        onClick={() => {
-          navigate(`/companyDetail/${company._id}`);
-        }}
-      >
-        Read more
-      </button>
+    <div className="containerPadre">
+      <div className="containerCompany">
+        <h6 className="companyName">Name: {company.companyName} </h6>
+        <h6 className="companyType"> Type:{company.type}</h6>
+        <h6 className="companyServices">Services offered:{company.companyServices}</h6>
+        <div className="imgCompany1">
+          <img className="imgCompany" src={company.image} alt={company.name} />
+        </div>
+        <LikeCompany id={company._id} />
+        <button
+          className="button--blue"
+          onClick={() => {
+            navigate(`/companyDetail/${company._id}`);
+          }}
+        >
+          Read more
+        </button>
+      </div>
     </div>
   );
 };

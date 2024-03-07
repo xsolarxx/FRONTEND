0ab5f00171e23afa57b02c9898acec19c97f008f
Monique Mendes <moniiquemendes96@gmail.com>
2024-03-07 23:08:21 +0100

    like + deslike

--- src/components/Cards/CompanyCard.jsx
+++ src/components/Cards/CompanyCard.jsx
@@ -5,11 +5,11 @@ import { LikeCompany } from '../LikeButtonCompany/LikeButton';
 export const CompanyCard = ({ company }) => {
   return (
     <div className="containerCompany">
-      <h3 className="companyName">{company.companyName} </h3>
-      <p className="companyDescription"> {company.description}</p>
+      <h3 className="companyName">Name: {company.companyName} </h3>
+      <p className="companyDescription">description: {company.description}</p>
       <img className="imgCompany" src={company.image} alt={company.name} />
-      <h2 className="companyType"> {company.type}</h2>
-      <p className="companyServices">{company.companyServices}</p>
+      <h2 className="companyType"> Type:{company.type}</h2>
+      <p className="companyServices">Services offered:{company.companyServices}</p>
       <LikeCompany id={company._id} />
     </div>
   );
--- src/components/LikeButtonCompany/LikeButton.jsx
+++ src/components/LikeButtonCompany/LikeButton.jsx
@@ -8,19 +8,24 @@ import { toggleLikedCompany } from '../../services/user.service';
 
 export const LikeCompany = ({ id }) => {
   const { user, setUser } = useAuth();
-  console.log('entro', user);
+
   const [like, setLike] = useState(!!user.likedCompany?.find((item) => item === id));
 
   const handleLikeClick = async () => {
-    console.log('entro', like);
+    console.log('like', like);
     if (user) {
       const { token } = user;
-      const likedCompany = await toggleLikedCompany(id);
-      const userUpdate = { ...likedCompany.data.user, token };
+      let updatedLikedCompany;
+      if (like) {
+        updatedLikedCompany = await toggleLikedCompany(id, false); // Remove company from liked companies
+      } else {
+        updatedLikedCompany = await toggleLikedCompany(id, true); // Add company to liked companies
+      }
+      const userUpdate = { ...updatedLikedCompany.data.user, token };
       setUser(() => userUpdate);
       localStorage.removeItem('user');
       localStorage.setItem('user', JSON.stringify(userUpdate));
-      setLike(!!likedCompany.data.user.likedCompany.find((item) => item === id));
+      setLike(!like); // Toggle like state
     }
   };
 

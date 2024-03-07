aa630cc31caddccd71b53370df4b2f1f9341d20f
InesMedem <inesmedem93@gmail.com>
2024-03-07 21:46:54 +0100

    cambios a header y comienzo de dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -6,20 +6,29 @@ import { useAuth } from '../../context/authContext';
 
 export const Dashboard = () => {
   const { user } = useAuth(); // Utiliza el hook useAuth para acceder al contexto de autenticación
-  //*console.log('entro', user);
   return (
     <>
       <section className="welcomeUser">
-        <h4>Welcome to our App </h4>
-        <h4>{user.name}</h4>
-        {/* Utiliza la interpolación de cadena dentro de las comillas invertidas */}
-        <img className="pictureProfile" src={user.image} alt="foto User" />
+        <h2>
+          Welcome {}
+          <span
+            style={{
+              textDecoration: 'underline',
+              textDecorationColor: '#97f85b',
+              textDecorationThickness: '3px',
+            }}
+          >
+            {user.name}
+          </span>
+        </h2>
+
+        <img className="profile-photo" src={user.image} alt="foto User" />
       </section>
+
       <section className="favoriteCompany">
-        <p>
-          <strong>Mis Empresas Favoritas:</strong>
-        </p>
+        <h3>Mis Empresas Favoritas:</h3>
       </section>
+
       <section className="feedUser">
         <p>test feed</p>
       </section>

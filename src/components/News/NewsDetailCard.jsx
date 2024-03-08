198e1d6937f16c820fb53a2025c8bddf986771d3
Xiao Xue <anxxue@gmail.com>
2024-03-08 20:44:28 +0100

    Created News full card Css, added styles, centered title, img and short content

--- src/components/News/NewsDetailCard.jsx
+++ src/components/News/NewsDetailCard.jsx
@@ -1,20 +1,22 @@
 /* Noticia con todo el contenido, inclusive el texto entero. Se llega aquí 
 a través de hacer click en "Read More" */
-import './NewsCard.css';
+import './NewsDetailCard.css';
 
 export const NewsDetailCard = ({ newsData }) => {
   return (
-    <>
-      <div className="pageContainerNews">
-        <div className="containerNews">
-          <h1 className="newsTitle">{newsData.title} </h1>
-          <p className="newsShortContent"> {newsData.shortContent}</p>
-          <p className="newsFullContent"> {newsData.fullContent}</p>
-          <img className="imgNews" src={newsData.image} alt={newsData.name} />
-          <p className="newsAuthor">{newsData.author}</p>
-          <p className="newsTags">{newsData.tags}</p>
-        </div>
+    <div className="pageContainerNewsDetailed">
+      <div className="containerNewsDetailed">
+        <h2 className="newsTitleDetailed">{newsData.title} </h2>
+        <section className="newsShortContentDetailed">
+          <p> {newsData.shortContent}</p>
+        </section>
+        <section className="newsImage">
+          <img className="imgNewsDetailed" src={newsData.image} alt={newsData.name} />
+        </section>
+        <p className="newsFullContentDetailed"> {newsData.fullContent}</p>
+        <p className="newsAuthorDetailed">{newsData.author}</p>
+        <p className="newsTagsDetailed">{newsData.tags}</p>
       </div>
-    </>
+    </div>
   );
 };

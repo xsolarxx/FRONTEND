a035ba29eef41df3cd7dca599da8e9a0ba23fc57
Adrian <ortizdev@proton.me>
2024-03-13 00:24:14 +0100

    filtros news

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,23 +1,41 @@
 import './News.css';
-
 import { useEffect, useState } from 'react';
-
 import { NewsCard } from '../../components';
 import { getAll } from '../../services/news.service';
 
 export const NewsPage = () => {
   const [newsList, setNewsList] = useState();
+  const [selectedTag, setSelectedTag] = useState(''); // Estado para almacenar la etiqueta seleccionada
+
   const fetchNews = async () => {
     setNewsList(await getAll());
   };
+
   useEffect(() => {
     fetchNews();
   }, []);
 
+  const handleTagFilter = (tag) => {
+    setSelectedTag(tag);
+  };
+
   return (
     <section className="NewsPageGrid">
+      <div>
+        <button className = "button--blue" onClick={() => handleTagFilter('')}>No filter</button>
+        <button className = "button--blue" onClick={() => handleTagFilter('Solar panels')}>Solar Panels</button>
+        <button className = "button--blue" onClick={() => handleTagFilter('Wind power')}>Wind power</button>
+         <button className ="button--blue"onClick={() => handleTagFilter('Others')}>Others</button>
+      </div>
       {newsList &&
-        newsList.data.map((news, index) => <NewsCard news={news} key={index} />)}
+        newsList.data
+          .filter((news) => {
+            return selectedTag === '' || news.tags.includes(selectedTag);
+          })
+          .map((news, index) => (
+            <NewsCard news={news} key={index} />
+          ))}
     </section>
   );
 };
+

2dc4dcd9017b11077ad8d56662f43ec3ac3520b2
Xiao Xue <anxxue@gmail.com>
2024-03-12 16:18:09 +0100

    Css of CompanyDetailCard and CompanyCard + Created new Css in index

--- src/components/Cards/ComapanyDetailCard.jsx
+++ src/components/Cards/ComapanyDetailCard.jsx
@@ -11,18 +11,22 @@ export const CompanyDetailCard = ({ company }) => {
       <div className="sectionCompanyText">
         <h2 className="sectionCompanyName"> {company.companyName} </h2>
         <h4 className="sectionCompanyDescription">{company.description}</h4>
+        <div className="companyContactDetails">
+          <label className="companyPhone">
+            <a href={`tel:+34${company.phoneNumber}`}>
+              Phone number: {company.phoneNumber}
+            </a>
+          </label>
+          <label className="companyEmail">
+            <a href={`mailto:${company.email}`}>Email address: {company.email}</a>
+          </label>
+        </div>
       </div>
       <div className="companyTagsDetailed">
-        <p className="companyTypeDetailed">Type: {company.companyType}</p>
-        <p className="companyServicesDetailed">Services offered : {company.companyServices}</p>
-        <div className="companyContactDetails">
-          <a href={`tel:+34${company.phoneNumber}`} className="companyPhone">
-            {company.phoneNumber}
-          </a>
-          <a href={`mailto:${company.email}`} className="companyPhone">
-            {company.email}
-          </a>
-        </div>
+        <label className="companyTypeDetailed">Type: {company.companyType}</label>
+        <label className="companyServicesDetailed">
+          Services offered : {company.companyServices}
+        </label>
       </div>
     </div>
   );

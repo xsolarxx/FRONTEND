43b3b0bca4a32983b45bae196927de7c866bfd77
Monique Mendes <moniiquemendes96@gmail.com>
2024-03-12 01:24:10 +0100

    Update comment

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { useCommentDeletion } from '../../hooks/useCommentDeletion'; // Importa la función correctamente
 
 export const Comments = ({ comment }) => {
   const creationDate = new Date(comment.createdAt);
   const formattedDate = creationDate.toLocaleString('es-ES', {
     timeZone: 'Europe/Madrid',
   });
-
-  // Llama al hook useCommentDeletion para obtener la función deleteComment
-  const { deleteComment } = useCommentDeletion();
 
   return (
     <>
@@ -17,7 +13,6 @@ export const Comments = ({ comment }) => {
         <p>{formattedDate}</p>
         <p>{comment.title}</p>
         <p>{comment.content}</p>
-        <button onClick={() => deleteComment(comment._id)}>Eliminar Comentario</button>
       </div>
     </>
   );

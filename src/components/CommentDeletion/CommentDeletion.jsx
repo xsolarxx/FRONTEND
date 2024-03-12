245c544d49dd4ba9829860eb9548cd987a06380a
Monique Mendes <moniiquemendes96@gmail.com>
2024-03-12 13:31:19 +0100

    quitando algunos codigos sin usar

--- src/components/CommentDeletion/CommentDeletion.jsx
+++ src/components/CommentDeletion/CommentDeletion.jsx
@@ -1,5 +1,4 @@
 import { deleteComment } from '../../services/comment.service';
-import { useState, useEffect } from 'react';
 import { useAuth } from '../../context/authContext';
 
 export const CommentDeletion = ({ idComment, onDeleteSuccess, setUpdateComments }) => {
@@ -43,65 +42,3 @@ export const CommentDeletion = ({ idComment, onDeleteSuccess, setUpdateComments 
     </>
   );
 };
-
-//! CODIGO UNO
-// export const useCommentDeletion = (id) => {
-//   const { user, setUser } = useAuth();
-
-//   const [comments, setComments] = useState(!!user.comments?.find((item) => item === id));
-
-//   const handleDeleteComment = async () => {
-//     console.log('entro', comments);
-//     if (user) {
-//       const { token } = user;
-//       const res = deleteComment(id);
-//       const userUpdate = {
-//         name: res.data.user.userName,
-//         email: res.data.user.email,
-//         image: res.data.user.image,
-//         check: res.data.user.check,
-//         _id: res.data.user._id,
-//         comments: res.data.user.comments,
-//         likedCompany: res.data.user.likedCompany,
-//         likedForum: res.data.user.likedForum,
-//         likedNews: res.data.user.likedNews,
-//         token,
-//       };
-//       setUser(() => userUpdate);
-//       localStorage.removeItem('user');
-//       localStorage.setItem('user', JSON.stringify(userUpdate));
-//       setComments(!!res.data.user.comments.find((item) => item === id));
-//     }
-//   };
-//   return (
-//     <div className="likebuttonCompany">
-//       {user && <button onClick={handleDeleteComment}>Delete</button>}
-//     </div>
-//   );
-// };
-
-//! CODIGO DOS
-// initialComments es un array de comentarios inicial
-// handleDeleteComment, que é chamada para excluir um comentário recebe o commentId
-// deleteComment llamos el servicio
-//
-
-// export const useCommentDeletion = () => {
-//   const [comments, setComments] = useState([]);
-
-//   const handleDeleteComment = async (commentId) => {
-//     try {
-//       await deleteComment(commentId);
-//       setComments((prevComments) =>
-//         prevComments.filter((comment) => comment._id !== commentId),
-//       );
-//     } catch (error) {
-//       console.error('Error al eliminar el comentario:', error);
-//     }
-//   };
-
-//   return {
-//     comments,
-//     deleteComment: handleDeleteComment,
-//   };
-// };

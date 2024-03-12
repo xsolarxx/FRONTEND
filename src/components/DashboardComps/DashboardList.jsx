5288a1885c19299a3012c6394211514a6db476cc
InesMedem <inesmedem93@gmail.com>
2024-03-12 20:21:13 +0100

    dahsboard progress css

--- src/components/DashboardComps/DashboardList.jsx
+++ src/components/DashboardComps/DashboardList.jsx
@@ -1,11 +1,12 @@
 import './DashboardList.css';
-export const DashboardList = ({ name, image, content, email }) => {
+
+export const DashboardList = ({ name, image, content, email, title }) => {
   return (
     <div className="DashboardList">
-      <img className="DashboardList_icon" src={image} alt="" />
+      {image && <img className="DashboardList_icon" src={image} alt="" />}
       <h4> {name} </h4>
       <p> {content}</p>
-      <p> {email}</p>
+      <p> {email} </p>
     </div>
   );
 };

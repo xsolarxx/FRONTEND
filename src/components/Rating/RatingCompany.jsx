8124b7608e04e3bda2d2bd45850a1075e039264c
InesMedem <inesmedem93@gmail.com>
2024-03-13 11:58:56 +0100

    Rating de Monique

--- src/components/Rating/RatingCompany.jsx
+++ src/components/Rating/RatingCompany.jsx
@@ -1,58 +1,57 @@
 import React, { useState, useEffect } from 'react';
+import { Rating } from 'primereact/rating';
 import { createRating } from '../../services/rating.service';
 import { useAuth } from '../../context/authContext';
 
-export const RatingComponent = ({ companyId }) => {
+export const RatingComponent = ({ idCompany }) => {
   const { user, setUser } = useAuth();
-  console.log('entro', user);
-
   const [rating, setRating] = useState(
-    !!user.companyPunctuated?.find((item) => item === companyId),
+    user.companyPunctuated.find((item) => item === idCompany),
   );
 
-  const handleRatingClick = async (value) => {
-    const formData = { punctuation: value, companyPunctuated: companyId };
-    if (user) {
-      try {
-        const res = await createRating(formData);
-        console.log('entro resssss', res);
-        const { token } = user;
-        const updatedUser = {
-          name: res.data.user.userName,
-          email: res.data.user.email,
-          image: res.data.user.image,
-          check: res.data.user.check,
-          _id: res.data.user._id,
-          companyPunctuated: res.data.user.companyPunctuated,
-          ownerRating: res.data.user.ownerRating,
-          likedCompany: res.data.user.likedCompany,
-          likedForum: res.data.user.likedForum,
-          likedNews: res.data.user.likedNews,
-          token,
-        };
-        setUser(() => updatedUser);
-        localStorage.removeItem('user');
-        localStorage.setItem('user', JSON.stringify(updatedUser));
-        setRating(!!res.data.user.companyPunctuated.find((item) => item === companyId));
-      } catch (error) {
-        console.error('Error saving rating:', error);
-      }
+  const handleRatingChange = async (e) => {
+    console.log('rating', rating);
+    setRating(e.value); // Actualiza el estado con la nueva calificación
+
+    try {
+      const formData = {
+        punctuation: e.value,
+        companyPunctuated: idCompany,
+      };
+
+      const res = await createRating(formData);
+      console.log('soy la respuesta', res);
+      const { token } = user;
+      const updatedUser = {
+        name: res?.data?.user?.userName,
+        email: res?.data?.user?.email,
+        image: res?.data?.user?.image,
+        check: res?.data?.user?.check,
+        _id: res?.data?.user?._id,
+        likedCompany: res?.data?.user?.likedCompany,
+        comments: res?.data?.user?.comments,
+        companyPunctuated: res?.data?.user?.companyPunctuated,
+        ownerRating: res?.data?.user?.ownerRating,
+        favComments: res?.data?.user?.favComments,
+        likedForum: res?.data?.user?.likedForum,
+        likedNews: res?.data?.user?.likedNews,
+        forumOwner: res?.data?.user?.forumOwner,
+        forumFollowing: res?.data?.user?.forumFollowing,
+        usersFollowed: res?.data?.user?.usersFollowed,
+        token,
+      };
+      setUser(() => updatedUser);
+
+      console.log('Calificación creada en el servidor:', res);
+    } catch (error) {
+      console.error('Error al crear la calificación:', error);
+      // Maneja el error según sea necesario
     }
   };
 
   return (
-    <div>
-      {[...Array(5)].map((_, index) => (
-        <button
-          key={index}
-          onClick={() => handleRatingClick(index + 1)}
-          disabled={!user || rating}
-          style={{ color: index < rating ? '#f90' : '#aaa' }}
-        >
-          &#9733;
-        </button>
-      ))}
-      {user && <p>{rating}</p>}
+    <div className="card flex justify-content-center">
+      <Rating value={rating} onChange={handleRatingChange} cancel={false} />
     </div>
   );
 };

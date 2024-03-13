6e33ab2e1d41e2f71d702f4cbfede043162e3593
Monique Mendes <moniiquemendes96@gmail.com>
2024-03-14 00:56:59 +0100

    css news detail

--- src/components/News/NewsDetailCard.jsx
+++ src/components/News/NewsDetailCard.jsx
@@ -4,22 +4,19 @@ import './NewsDetailCard.css';
 import { LikeNews } from '../../components/LikeButtonCompany/LikeButtonNews';
 export const NewsDetailCard = ({ newsData }) => {
   return (
-    <div className="pageContainerNewsDetailed">
-      <div className="containerNewsDetailed">
-        <h2 className="newsTitleDetailed">{newsData.title} </h2>
-        <section className="newsShortContentDetailed">
-          <p> {newsData.shortContent}</p>
-        </section>
-        <section className="newsImageDetailed">
-          <img className="imgNewsDetailed" src={newsData.image} alt={newsData.name} />
-        </section>
-        <p className="newsFullContentDetailed"> {newsData.fullContent}</p>
-        <div className="authorAndTags">
-          <label className="newsAuthorDetailed">By: {newsData.author}</label>
-          <label className="newsTagsDetailed">Tag: {newsData.tags}</label>
-        </div>
+    <>
+      <div className="pageContainerNewsDetailed">
+        <h1 className="newsTitleDetailed">{newsData.title} </h1>
+
+        <img src={newsData.image} alt={newsData.name} />
+
+        <p> {newsData.fullContent}</p>
+
+        <label className="newsAuthorDetailed">By: {newsData.author}</label>
+        <label className="newsTagsDetailed">Tag: {newsData.tags}</label>
+
+        <LikeNews id={newsData?._id} />
       </div>
-      <LikeNews id={newsData?._id} />
-    </div>
+    </>
   );
 };

32eb50b63808be6664ff45553a8a664d41aed8cc
InesMedem <inesmedem93@gmail.com>
2024-03-14 23:47:01 +0100

    news page responsive

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -20,27 +20,32 @@ export const NewsPage = () => {
   };
 
   return (
-    <section className="NewsPageGrid">
-      {/* <div className="tagsfilter">
-        <button className="button--blue" onClick={() => handleTagFilter('')}>
-          No filter
-        </button>
-        <button className="button--blue" onClick={() => handleTagFilter('Solar panels')}>
-          Solar Panels
-        </button>
-        <button className="button--blue" onClick={() => handleTagFilter('Wind power')}>
-          Wind power
-        </button>
-        <button className="button--blue" onClick={() => handleTagFilter('Others')}>
-          Others
-        </button>
-      </div> */}
-      {newsList &&
-        newsList.data
-          .filter((news) => {
-            return selectedTag === '' || news.tags.includes(selectedTag);
-          })
-          .map((news, index) => <NewsCard news={news} key={index} />)}
-    </section>
+    <>
+      <section className="NewsPageGrid">
+        <div className="tagsfilter">
+          <button className="button--blue" onClick={() => handleTagFilter('')}>
+            No filter
+          </button>
+          <button
+            className="button--blue"
+            onClick={() => handleTagFilter('Solar panels')}
+          >
+            Solar Panels
+          </button>
+          <button className="button--blue" onClick={() => handleTagFilter('Wind power')}>
+            Wind power
+          </button>
+          <button className="button--blue" onClick={() => handleTagFilter('Others')}>
+            Others
+          </button>
+        </div>
+        {newsList &&
+          newsList.data
+            .filter((news) => {
+              return selectedTag === '' || news.tags.includes(selectedTag);
+            })
+            .map((news, index) => <NewsCard news={news} key={index} />)}
+      </section>
+    </>
   );
 };

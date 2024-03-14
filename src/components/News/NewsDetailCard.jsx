db8cf9e514a14486c9f36e3801542715511f5a85
Monique Mendes <moniiquemendes96@gmail.com>
2024-03-14 11:47:23 +0100

    Css ok news detail

--- src/components/News/NewsDetailCard.jsx
+++ src/components/News/NewsDetailCard.jsx
@@ -1,20 +1,20 @@
 /* Noticia en grande con todo el contenido, inclusive el texto entero. Se llega aquí 
 a través de hacer click en "Read More" */
 import './NewsDetailCard.css';
+
 import { LikeNews } from '../../components/LikeButtonCompany/LikeButtonNews';
 export const NewsDetailCard = ({ newsData }) => {
   return (
     <>
       <div className="pageContainerNewsDetailed">
         <h1>{newsData.title} </h1>
-        <p className="newsShortContent"> {newsData.shortContent}</p>
+        <p id="newsShortContent"> {newsData.shortContent}</p>
         <img src={newsData.image} alt={newsData.name} />
-
         <p> {newsData.fullContent}</p>
-
-        <label className="newsAuthorDetailed">By: {newsData.author}</label>
-        <label className="newsTagsDetailed">Tag: {newsData.tags}</label>
-
+        <div id="authorAndTag">
+          <label className="newsAuthor">By: {newsData.author}</label>
+          <label className="newsTags">Tag: {newsData.tags}</label>
+        </div>
         <LikeNews id={newsData?._id} />
       </div>
     </>

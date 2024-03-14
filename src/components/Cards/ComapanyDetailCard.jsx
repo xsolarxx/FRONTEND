b6d935b316d70cf38fe27bbc6597fe1c1d38c340
Monique Mendes <moniiquemendes96@gmail.com>
2024-03-14 23:55:06 +0100

    Modificaciones css companydetail

--- src/components/Cards/ComapanyDetailCard.jsx
+++ src/components/Cards/ComapanyDetailCard.jsx
@@ -8,42 +8,41 @@ import { LikeCompany } from '../LikeButtonCompany/LikeButton';
 export const CompanyDetailCard = ({ company }) => {
   console.log('detail', company);
   return (
-    <div className="containerCompanyDetailed">
-      <img className="sectionCompanyImg" src={company.image} alt={company.name} />
-      <div className="sectionCompanyText">
-        <h2 className="sectionCompanyName"> {company.companyName} </h2>
-        <p
-          className="sectionCompanyDescription"
-          dangerouslySetInnerHTML={{ __html: company.description }}
-        />
-        <div className="companyContactDetails">
-          <label className="companyPhone">
-            Phone number:{' '}
-            <a href={`tel:+34${company.phoneNumber}`}>
-              <b>{company.phoneNumber}</b>
-            </a>
-          </label>
-          <label className="companyEmail">
-            Email address:{' '}
-            <a href={`mailto:${company.email}`}>
-              <b>{company.email}</b>
-            </a>
+    <>
+      <div className="containerCompanyDetailed">
+        <img className="sectionCompanyImg" src={company.image} alt={company.name} />
+        <div className="sectionCompanyText">
+          <h2 className="sectionCompanyName"> {company.companyName} </h2>
+          <p
+            className="sectionCompanyDescription"
+            dangerouslySetInnerHTML={{ __html: company.description }}
+          />
+          <div className="companyContactDetails">
+            <label className="companyPhone">
+              Phone number:{' '}
+              <a href={`tel:+34${company.phoneNumber}`}>
+                <b>{company.phoneNumber}</b>
+              </a>
+            </label>
+            <label className="companyEmail">
+              Email address:{' '}
+              <a href={`mailto:${company.email}`}>
+                <b>{company.email}</b>
+              </a>
+              <RatingComponent company={company} />
+            </label>
+          </div>
+        </div>
+        <div className="companyTagsDetailed">
+          <label className="companyTypeDetailed">Type: {company.companyType}</label>
+          <label className="companyServicesDetailed">
+            Services offered : {company.companyServices}
           </label>
         </div>
+        <div className="companyLikesCompany">
+          <LikeCompany id={company._id} />
+        </div>
       </div>
-      <div className="companyTagsDetailed">
-        <label className="companyTypeDetailed">Type: {company.companyType}</label>
-        <label className="companyServicesDetailed">
-          Services offered : {company.companyServices}
-        </label>
-      </div>
-      <div className="ratingContainer">
-        <h5>Rate this company:</h5>
-        <RatingComponent company={company} />
-      </div>
-      <div>
-        <LikeCompany id={company._id} />
-      </div>
-    </div>
+    </>
   );
 };

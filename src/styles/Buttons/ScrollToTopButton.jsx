08663a2371b3e95530af0621f0c9d438202d1af9
Monique Mendes <moniiquemendes96@gmail.com>
2024-03-14 01:35:13 +0100

    Tooltip scroll

--- src/styles/Buttons/ScrollToTopButton.jsx
+++ src/styles/Buttons/ScrollToTopButton.jsx
@@ -3,6 +3,7 @@ import './ScrollToTopButton.css'; // Estilos CSS para o botão
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons'; // Importe o ícone de seta para cima do Font Awesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Tooltip } from 'react-tooltip';
 
 export const ScrollToTopButton = () => {
   // Função para rolar a página para o topo
@@ -11,10 +12,16 @@ export const ScrollToTopButton = () => {
   };
 
   return (
-    <FontAwesomeIcon
-      icon={faArrowUp}
-      className="scrollToTopButton"
-      onClick={scrollToTop}
-    />
+    <>
+      <FontAwesomeIcon
+        icon={faArrowUp}
+        className="scrollToTopButton"
+        onClick={scrollToTop}
+        data-tooltip-id="my-tooltip"
+        data-tooltip-content="Scroll to the top of the page"
+        data-tooltip-variant="success"
+      />
+      <Tooltip id="my-tooltip" />
+    </>
   );
 };

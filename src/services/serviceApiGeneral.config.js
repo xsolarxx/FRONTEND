99f176fceeb9aa8b773da971335eb08162f609fb
InesMedem <inesmedem93@gmail.com>
2024-03-18 08:44:37 +0100

    vercel.json with routes and base url update

--- src/services/serviceApiGeneral.config.js
+++ src/services/serviceApiGeneral.config.js
@@ -2,16 +2,14 @@ import axios from 'axios';
 
 import { updateToken } from '../utils/updateToken';
 
-/**
- * Tenemos que cambiar la URL
- *
- */
 export const extraConfig = () => {
   return axios.create({
-    baseURL: 'https://xsolarx-gtf92f30z-xsolarxs-projects.vercel.app/api/v1',
+    baseURL: 'https://xsolarx-71hfd923x-xsolarxs-projects.vercel.app',
+
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
       Authorization: `Bearer ${updateToken()}`,
     },
     timeout: 60000,
